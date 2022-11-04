324c8ba07a56ecafcb21388d947359e3235fe23a
Funke Faith Olasupo <fizzyflexy397@gmail.com>
2022-11-04 09:51:07 +0100

    fix:validate to make email unique

--- inventorbackend/database/migrations/2022_11_03_121719_create_join_us_table.php
+++ inventorbackend/database/migrations/2022_11_03_121719_create_join_us_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('join_us', function (Blueprint $table) {
             $table->id();
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('track');
             $table->string('portfolio')->nullable();
             $table->timestamps();
